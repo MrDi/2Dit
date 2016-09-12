1d66618bb506a52d5316fc67d26cf51826df6cca
Mr_di7 <fan_man_dimon@mail.ru>
2016-09-12 18:15:56 +0300

    16
    По кругу

--- js/examples.js
+++ js/examples.js
@@ -1,18 +1,26 @@
 $(document).ready(function () {
 
     var $right_block = $(".right-block"),
-     $note = $("img.note"),
-      $right_second =  $(".right-second"),
-        $header = $(".header"),
-        $img_page = $(".img_page1,.img_page2,.img_page3,.img_page4");
-var $layout1 = document.querySelector(".img_page1"),
+    $note = $("img.note"),
+    $right_second =  $(".right-second"),
+    $header = $(".header"),
+    $img_page = $(".img_page1,.img_page2,.img_page3,.img_page4");
+    
+    var $layout1 = document.querySelector(".img_page1"),
     $layout2 = document.querySelector(".img_page2"),
     $layout3 = document.querySelector(".img_page3"),
     $layout4 = document.querySelector(".img_page4"),
     $myBtn = document.querySelector(".button"),
     $myForm = document.querySelector(".form_left_modal"),
     $fullPage = document.querySelector(".modal_back"),
-    $button_fix = document.querySelector(".button_fix");
+    $button_fix = document.querySelector(".button_fix"),
+        $case_bool = false,
+    $slide_finish = false,
+        $slide_first = true;
+    
+
+    
+    
 
     $('#fullpage').fullpage({
         menu: '#menu',
@@ -23,22 +31,20 @@ var $layout1 = document.querySelector(".img_page1"),
         scrollBar: true,
         loopHorizontal: true,
         navigation: true,
-        resetSliders:true,
-        lockAnchors:true,
+        resetSliders: true,
+        lockAnchors: true,
         navigationPosition: 'right',
+        ////////////////////////////////////////////////////////////////////
         onLeave: function (index, nextIndex, direction) {
+              console.log('onLeave:Я покинул страницу'+ index);
+              console.log('onLeave:Я нахожусь на странице'+ nextIndex);
             if (nextIndex == 2) {
                 $(".fp-controlArrow.fp-prev").css('display', 'none');
 
-                //$.fn.fullpage.moveSlideRight();
-                //return false;
             }
 
             if (nextIndex == 2) {
                 $right_second.addClass('anim_sheet');
-                /*  $(".header").addClass('header-fixed');*/
-
-
             }
 
             if (nextIndex == 3) {
@@ -46,42 +52,134 @@ var $layout1 = document.querySelector(".img_page1"),
                 $("#fp-nav").css('display', 'none');
                if(direction == 'down'){
                     $(".fp-controlArrow.fp-next").fadeOut();
-               }
-                /*  $(".header").addClass('header-fixed');direction:down*/
-
-
+               } 
             }
-
-
+            
             if (nextIndex == 1) {
 
               $header.removeClass('header-fixed');
 
 
             }
+            
+            if (nextIndex == 3 && direction=='up') {
+
+              /* $case_bool=true;*/
+            }
+               if (nextIndex == 3){
+                   $case_bool = true;  
+               }
+            
+           
+             if (nextIndex == 4) {
+                
+               
+             
+               if($case_bool && !$slide_finish){
+                 
+                   if(direction == 'up'){
+                     $.fn.fullpage.moveSlideLeft();
+                   }
+                   else
+                       if(direction == 'down'){
+                          $.fn.fullpage.moveSlideRight();
+                          
+                       }
+                    return false;
+               }
+                 
+                 
+                   }
+            
+            if(index == 3 && nextIndex == 2 ){
+                
+                 if($case_bool && !$slide_first){
+                     
+                      if(direction == 'up'){
+                     $.fn.fullpage.moveSlideLeft();
+                   }
+                   else
+                       if(direction == 'down'){
+                          $.fn.fullpage.moveSlideRight();
+                          
+                       }
+                    return false;
+                 }
+                
+            }
+               /*  if($slide_finish){
+                
+                      if(direction=='up'){
+                     $.fn.fullpage.moveSlideLeft();
+                         
+                   }
+                   else{
+                      $.fn.fullpage.moveSlideRight();
+                   }
+                    return false;
+               
+            }*/
 
         },
         afterLoad: function (anchorLink, index) {
-
+             console.log('afterLoad:Я пришел на страницу'+ index);
+             
+            
             if (index != 3) {
                 $("#fp-nav").css('display', 'block');
+               
+                
             }
 
             if (index != 1) {
 
                $header.addClass('header-fixed');
             }
+            
+            
+            
 
         },
         onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
 
-
+  
+            console.log('onSlideLeave:Я покинул слайд'+ slideIndex);
+            console.log('onSlideLeave:Я пришел на слайд'+ nextSlideIndex);
             if (nextSlideIndex == 0) {
                 $(".fp-controlArrow.fp-next").fadeOut();
+              /*  $case_bool=false;
+                $slide_first=true;*/
+            }
+            
+            if(slideIndex == 0 && nextSlideIndex == 1){
+                $slide_first = false;
+            }
+            
+            
+            if(slideIndex == 4 && nextSlideIndex == 0){
+            $slide_finish = true;
+            }
+            else{
+                $slide_finish = false;
+            }
+           
+             if(slideIndex == 1 && nextSlideIndex == 0){
+                
+                 $slide_first = true;
+            }
+            
+            if (nextSlideIndex != 0) {
+           
+                
             }
             
             if (nextSlideIndex == 1) {
               $note.after('<img class="note_gif" src="images/notebook_landind.gif" />');
+           /* $case_bool = true;*/
+           
+           
+                
+                
             }
             
             if (nextSlideIndex == 3) {
@@ -89,27 +187,31 @@ var $layout1 = document.querySelector(".img_page1"),
             }
             
             if (nextSlideIndex == 4) {
-             //   $img_page.addClass('anim');
+           
             
                 $layout1.classList.add("anim");
                 $layout2.classList.add("anim");
                 $layout3.classList.add("anim");
                 $layout4.classList.add("anim");
+            
+                
+            
                 
             }
 
             if (nextSlideIndex == 0 && slideIndex == 4) {
-  /* $(".img_page1,.img_page2,.img_page3,.img_page4").removeClass('anim');
-                if($(".img_page1").*/
+               
                 $.fn.fullpage.moveSectionDown();
                 return false;
+                
             }
-
+            
+           
+                
             if (nextSlideIndex == 4 && slideIndex == 0) {
                 $(".fp-controlArrow.fp-prev").css('display', 'none');
-
                 $.fn.fullpage.moveSectionUp();
-                return false;//.notebook.right anim_note
+                return false;
             }
         },
         afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
@@ -118,10 +220,10 @@ var $layout1 = document.querySelector(".img_page1"),
                 $(".fp-controlArrow.fp-next").fadeIn();
             }
             
-            //note
-             /*if (slideIndex == 1) {
-               $(".note").after('<img class="note_gif" src="images/notebook_landind.gif" />');
-            }*/
+            if(slideIndex==4){
+                console.log('последйний слайд');
+                $slide_finish=true;
+            }
         }
     });
 
